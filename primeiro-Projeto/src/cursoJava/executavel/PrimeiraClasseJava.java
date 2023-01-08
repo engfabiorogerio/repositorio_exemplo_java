ccb13d2ac0e08a02edc32b468dda905fbfe1bc37
Fábio Rogério de Moraes e Silva <102857097+engfabiorogerio@users.noreply.github.com>
2023-01-08 10:21:57 -0200

    Initial Commit

---
+++ primeiro-Projeto/src/cursoJava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,8 @@
+package cursoJava.executavel;
+
+public class PrimeiraClasseJava {
+	/*modo para comentários*/
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
